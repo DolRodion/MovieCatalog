c1fc7b72186bb2b6a7e933e59339e1d55626352e
Rodion <dolrodion@gmail.com>
2022-07-19 14:28:10 +0300

    first commit

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,9 +4,10 @@ import router from './router';
 import store from './store';
 import vuetify from './plagins/vuetify';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuetify);
 
 new Vue({
   router,
